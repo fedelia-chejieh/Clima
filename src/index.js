e884f809e6218ca4e7c61237815ba42294bc0b60
Fedelia Chejieh <fedelia.cj@gmail.com>
2024-03-02 19:02:11 +0100

    Injected the forcast HTML using JavaScript

--- src/index.js
+++ src/index.js
@@ -49,7 +49,34 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+          <div class="weather-forecast-date">${day}
+          </div>
+          <img src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"
+            class="weather-forecast-icon">
+          <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+              <strong>10°</strong>
+            </div>
+            <div class="weather-forecast-temperature">5°</div>
+          </div>
+        </div>`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity(`Paris`);
+
+displayForecast();
